ea5730ea5bc074066b6ce86747904bee71746986
Mbabazi Alvin <alvineald@gmail.com>
2026-07-02 23:01:06 +0300

    feat: improve logout functionality by ensuring session data and cookies are cleared

--- logout.php
+++ logout.php
@@ -7,10 +7,23 @@ session_start();
 require_once 'includes/functions.php';
 
 // Clear all session data
+$_SESSION = [];
 session_unset();
+
+// Delete the session cookie from the browser (not just the data)
+if (ini_get('session.use_cookies')) {
+    $p = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $p['path'], $p['domain'], $p['secure'], $p['httponly']);
+}
+
 session_destroy();
+
+// Make sure nothing about this response is cached
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
 
 // Redirect to login with message
 header("Location: login.php?msg=You+have+been+signed+out+successfully");
 exit();
-?>
